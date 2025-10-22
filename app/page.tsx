842da3cc056cbc76f4c7756230eeb4b054872a28
Seunghan Ko <24_10601@dshs.kr>
2025-10-22 21:53:23 +0900

    change 11-error

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function Page() {
         <Hero />
         <ProductsSection />
         <SaenaFeatures />
-        {/* <ScrollStackSection /> */}
+        <ScrollStackSection />
         <UseCasesSwap />
         <TeamSection />
         <Pricing />
